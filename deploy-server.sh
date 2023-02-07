a23e489923909edfedb0874982e457b1d2d1db95
Gabriel Jablonski <gabriel.g.jablonski@gmail.com>
2023-02-07 19:08:04 -0300

    Update deploy-server.sh

--- deploy-server.sh
+++ deploy-server.sh
@@ -44,7 +44,7 @@ module.exports = {
     name   : \"fazenda-nota-server-$tag_name\",
     script : \"`pwd`/$tag_name/index.js\",
     error_file : \"`pwd`/error.log\",
-    out_file : \"`pwd`/out.log\"
+    out_file : \"`pwd`/out.log\",
     env: {
       DISPLAY: \":1\"
     }
