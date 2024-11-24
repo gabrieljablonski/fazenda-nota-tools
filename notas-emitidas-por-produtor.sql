012642c29455b5aac3ddc3e420081701e5e4b2bf
Gabriel Jablonski <gabriel.g.jablonski@gmail.com>
2024-11-24 13:58:34 -0300

    Update notas-emitidas-por-produtor.sql

--- notas-emitidas-por-produtor.sql
+++ notas-emitidas-por-produtor.sql
@@ -3,8 +3,9 @@ select
   (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2023-01-01' and DATE(n.issuedOn) < '2023-07-01') as '2023 S1',
   (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2023-06-30' and DATE(n.issuedOn) < '2024-01-01') as '2023 S2',
   (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'CANCELLED' and DATE(n.issuedOn) > '2023-01-01' and DATE(n.issuedOn) < '2024-01-01') as 'Canceladas 2023',
-  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2023-12-31') as '2024',
-  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'CANCELLED' and DATE(n.issuedOn) > '2023-12-31') as 'Canceladas 2024',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2024-01-01' and DATE(n.issuedOn) < '2024-07-01') as '2024 S1',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2024-06-30' and DATE(n.issuedOn) < '2025-01-01') as '2024 S2',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'CANCELLED' and DATE(n.issuedOn) > '2023-12-31' and DATE(n.issuedOn) < '2025-01-01') as 'Canceladas 2024',
   p.addressState, p.addressCity, p.addressCep, p.addressStreet, p.addressNumber, p.addressExtra 
 from Person as p where p.type = 'PROPERTY' order by p.legalName
 INTO OUTFILE '/var/lib/mysql-files/notas-emitidas-2024-s1.csv'
