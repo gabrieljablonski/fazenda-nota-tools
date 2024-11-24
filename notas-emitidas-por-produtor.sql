d701c8b139d977a89271c00c15ad8d3ca4ff001f
Gabriel Jablonski <gabriel.g.jablonski@gmail.com>
2024-11-24 13:52:22 -0300

    Add files via upload

---
+++ notas-emitidas-por-produtor.sql
@@ -0,0 +1,13 @@
+select 
+  p.id, p.legalName, p.cpfCnpj, p.stateRegistration, p.nfpeNumber,
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2023-01-01' and DATE(n.issuedOn) < '2023-07-01') as '2023 S1',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2023-06-30' and DATE(n.issuedOn) < '2024-01-01') as '2023 S2',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'CANCELLED' and DATE(n.issuedOn) > '2023-01-01' and DATE(n.issuedOn) < '2024-01-01') as 'Canceladas 2023',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2023-12-31') as '2024',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'CANCELLED' and DATE(n.issuedOn) > '2023-12-31') as 'Canceladas 2024',
+  p.addressState, p.addressCity, p.addressCep, p.addressStreet, p.addressNumber, p.addressExtra 
+from Person as p where p.type = 'PROPERTY' order by p.legalName
+INTO OUTFILE '/var/lib/mysql-files/notas-emitidas-2024-s1.csv'
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n';
