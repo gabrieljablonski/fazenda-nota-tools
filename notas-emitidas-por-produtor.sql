0989ab014e8a35d882cb4b8df506bbfb1563399d
Gabriel Jablonski <gabriel.g.jablonski@gmail.com>
2025-06-15 13:05:57 -0300

    Update notas-emitidas-por-produtor.sql

--- notas-emitidas-por-produtor.sql
+++ notas-emitidas-por-produtor.sql
@@ -6,9 +6,12 @@ select
   (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2024-01-01' and DATE(n.issuedOn) < '2024-07-01') as '2024 S1',
   (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2024-06-30' and DATE(n.issuedOn) < '2025-01-01') as '2024 S2',
   (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'CANCELLED' and DATE(n.issuedOn) > '2023-12-31' and DATE(n.issuedOn) < '2025-01-01') as 'Canceladas 2024',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2025-01-01' and DATE(n.issuedOn) < '2025-07-01') as '2025 S1',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'SENT' and DATE(n.issuedOn) > '2025-06-30' and DATE(n.issuedOn) < '2026-01-01') as '2025 S2',
+  (select COUNT(*) from Nfpe as n where n.issuedById = p.id and n.status = 'CANCELLED' and DATE(n.issuedOn) > '2024-12-31' and DATE(n.issuedOn) < '2026-01-01') as 'Canceladas 2025',
   p.addressState, p.addressCity, p.addressCep, p.addressStreet, p.addressNumber, p.addressExtra 
 from Person as p where p.type = 'PROPERTY' order by p.legalName
-INTO OUTFILE '/var/lib/mysql-files/notas-emitidas-2024-s1.csv'
+INTO OUTFILE '/var/lib/mysql-files/notas-emitidas-2025-s1.csv'
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
